e0ba5c15fe69eed9bf8a8120fc141309465b5c46
João Vitor <82537300+joaoafb@users.noreply.github.com>
2022-06-24 13:38:15 -0300

    Add files via upload

--- assets/app.js
+++ assets/app.js
@@ -1,13 +1,11 @@
-changetxt()
+
 function changetxt(){
     setInterval(() => {
         if(document.getElementById("txt").textContent == "Social Media Designer"){ 
-        document.getElementById("txt").innerHTML = "Desenvolvedor Web"
-        document.body.style.background = ' radial-gradient(circle at 1.8% 4.8%, rgb(17, 23, 58) 0%, rgb(58, 85, 148) 90%)'
-
+        document.getElementById("txt").innerHTML = "Desenvolvedor Web Front-End"
+       
         }else{
             document.getElementById("txt").innerHTML = "Social Media Designer"
-            document.body.style.background = ' linear-gradient(109.6deg, rgb(43, 1, 91) 13.4%, rgb(122, 2, 54) 100.2%)'
             
         }
     
@@ -16,17 +14,5 @@ function changetxt(){
 }
 
 
-function suave(){ 
-    window.scroll({       // 1
-      top: document
-      .querySelector('body' )
-        .offsetTop,       // 2
-      left: 0,
-      behavior: 'smooth'// 3
-     });
-  }
-  suave()
-  
-  function portfolio(){
-    this.scrollIntoView({ behavior: "smooth" });
-  }
+changetxt()
+
