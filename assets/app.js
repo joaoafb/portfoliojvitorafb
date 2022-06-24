dcece6134ba79915b8bd6d3e43d190a4221edcbb
João Vitor <82537300+joaoafb@users.noreply.github.com>
2022-06-24 19:45:19 -0300

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -1,18 +1,18 @@
 
-function changetxt(){
+
     setInterval(() => {
         if(document.getElementById("txt").textContent == "Social Media Designer"){ 
-        document.getElementById("txt").innerHTML = "Desenvolvedor Web Front-End" + '<span class="inter">!</span>'
+        document.getElementById("txt").innerHTML = "Desenvolvedor Web Front-End" 
        
-        }else{
-            document.getElementById("txt").innerHTML = "Social Media Designer" + '<span class="inter">!</span>'
-            
         }
+        else{
+            document.getElementById("txt").innerHTML = "Social Media Designer" 
+        }
+        
     
     }, 2000);
 
-}
 
 
-changetxt()
+
 
